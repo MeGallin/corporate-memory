ac3c5dc2e350a99692307e213319f95b12bf2c55
Guy <me@garyallin.uk>
2020-04-16 11:33:36 +0100

    Working: Bootstraping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RoutesModule } from './routes.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DateTimeComponent } from './shared/date-time/date-time.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, ProfileComponent, HeaderComponent, FooterComponent, DateTimeComponent],
+  imports: [BrowserModule, RoutesModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/routes.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      scrollPositionRestoration: 'enabled'
+    })
+  ],
+  exports: [RouterModule]
+})
+export class RoutesModule {}
